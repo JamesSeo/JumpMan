254e9f82a6ad5bb40f5e015b342538bd3cf12e74
JunHo Seo <kawada@JunHo-Seoui-MacBook-Pro.local>
2014-07-20 21:02:39 +0900

    init commit background layer

---
+++ Classes/BGLayer.cpp
@@ -0,0 +1,13 @@
+//
+//  BGLayer.cpp
+//  JumpMan
+//
+//  Created by JunHo Seo on 2014. 7. 20..
+//
+//
+
+#include "BGLayer.h"
+
+bool BGLayer::init(){
+    return true;
+}
---
+++ Classes/BGLayer.h
@@ -0,0 +1,24 @@
+//
+//  BGLayer.h
+//  JumpMan
+//
+//  Created by JunHo Seo on 2014. 7. 20..
+//
+//
+
+#ifndef __JumpMan__BGLayer__
+#define __JumpMan__BGLayer__
+
+#include <iostream>
+#include "cocos2d.h"
+
+USING_NS_CC;
+
+class BGLayer : public Layer {
+    
+    virtual bool init();
+    // implement the "static create()" method manually
+    CREATE_FUNC(BGLayer);
+};
+
+#endif /* defined(__JumpMan__BGLayer__) */
